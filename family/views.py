12de6ce02e677358389063c70d238e52abcb37b2
gek <rostergek@gmail.com>
2019-08-04 23:55:29 +0300

    that works

--- family/views.py
+++ family/views.py
@@ -1,15 +1,23 @@
 from .models import Parent,Child
 from django.views.generic import ListView
+from django.db.models import Prefetch
 
 
 class ParentListView(ListView):
     model = Parent
-    template_name = 'parent_list.html'
+    template_name = 'child_list.html'
     context_object_name = 'parent_objects_list'
 
 
 class ChildListView(ListView):
-    model = Child
+    model = Parent
     template_name = 'child_list.html'
-    context_object_name = 'child_objects_list'
-    queryset = Parent.objects.prefetch_related('child')
+    context_object_name = 'parent_child_objects_list'
+    queryset = Parent.objects.prefetch_related(Prefetch('child_set'))
+
+
+# class GChildListView(ListView):
+#     model = Child
+#     template_name = 'gchild_list.html'
+#     context_object_name = 'child_objects_list'
+#     queryset = Parent.objects.prefetch_related(Prefetch('child_set')).all()
